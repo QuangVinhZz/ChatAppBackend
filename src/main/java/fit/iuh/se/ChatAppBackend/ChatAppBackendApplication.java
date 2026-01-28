613444287addb30ef6e5b6c01226f389c660ea8a
Le Nguyen Quang Vinh <quangvinh7137@gmail.com>
2026-01-28 18:57:10 +0700

    first commit

---
+++ src/main/java/fit/iuh/se/ChatAppBackend/ChatAppBackendApplication.java
@@ -0,0 +1,13 @@
+package fit.iuh.se.ChatAppBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatAppBackendApplication.class, args);
+	}
+
+}
